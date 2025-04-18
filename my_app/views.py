3d41116ca986b9b487e4f4d98fab031e83ee0b7b
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-04-18 17:30:12 +0800

    Add files via upload

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm, BookForm
+from .forms import SignUpForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .models import Book, UserAccount, Post, Comment
